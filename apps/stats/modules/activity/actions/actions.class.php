d596ef2c932a9c6ca706d88d90392a68018982da
Fabrice-li <fabrice.petrequin@libre-informatique.fr>
2018-02-09 11:07:15 +0100

    (stats) - Change the activity query to take the oldest printed date - refs #5729 (#574)

--- apps/stats/modules/activity/actions/actions.class.php
+++ apps/stats/modules/activity/actions/actions.class.php
@@ -53,8 +53,24 @@ class activityActions extends sfActions
     
     switch ( $denomination ) {
     case 'hour':
+      $from = 'true';
+      $to = 'true';
+    
+      if ( $dates['from'] )
+        $from = " m.happens_at >= '".date('Y-m-d', $dates['from'])."' ";
+      if ( $dates['to'] )
+        $to = " m.happens_at < '".date('Y-m-d', $dates['to'])."' ";
+    
+      $sq = "SELECT tck.id, transaction_id, manifestation_id, min(printed_at) printed_at, min(integrated_at) integrated_at
+        FROM ticket_version tck
+        INNER JOIN manifestation m ON m.id = tck.manifestation_id 
+        WHERE true 
+        AND $from 
+        AND $to
+        GROUP BY tck.id, transaction_id, manifestation_id";
+    
       $q = "SELECT COUNT(DISTINCT tck.id) AS nb, date_part('hour', CASE WHEN tck.printed_at IS NOT NULL THEN tck.printed_at ELSE CASE WHEN tck.integrated_at IS NOT NULL THEN tck.integrated_at ELSE o.created_at END END) AS hour
-            FROM ticket tck
+            FROM ($sq) tck
             LEFT JOIN order_table o ON o.transaction_id = tck.transaction_id
             LEFT JOIN manifestation m ON m.id = tck.manifestation_id";
       
@@ -68,12 +84,6 @@ class activityActions extends sfActions
       $q .= $subfrom;
       $q .= " WHERE (tck.printed_at IS NOT NULL OR tck.integrated_at IS NOT NULL OR o.created_at IS NOT NULL)";
       $q .= $subwhere;
-      
-      if ( $dates['from'] )
-        $q .= " AND m.happens_at >= '".date('Y-m-d', $dates['from'])."' ";
-      if ( $dates['to'] )
-        $q .= " AND m.happens_at < '".date('Y-m-d', $dates['to'])."' ";
-      
       
       $q .= " GROUP BY date_part('hour', CASE WHEN tck.printed_at IS NOT NULL THEN tck.printed_at ELSE CASE WHEN tck.integrated_at IS NOT NULL THEN tck.integrated_at ELSE o.created_at END END)
               ORDER BY date_part('hour', CASE WHEN tck.printed_at IS NOT NULL THEN tck.printed_at ELSE CASE WHEN tck.integrated_at IS NOT NULL THEN tck.integrated_at ELSE o.created_at END END)";
@@ -96,8 +106,26 @@ class activityActions extends sfActions
       foreach ( $days as $key => $day )
         $days[$key] = date('Y-m-d',$day);
       
+      $sFrom1 = str_replace('%%cpt%%', '1', $subfrom);
+      $sWhere1 = str_replace('%%cpt%%', '1', $subwhere);
+      
       $q = "SELECT d.date, d.date + '$interval days'::interval AS end,
-            (SELECT count(DISTINCT tck1.id) FROM ticket tck1 ".str_replace('%%cpt%%', '1', $subfrom)." WHERE ".str_replace('%%cpt%%', '1', $subwhere)." (tck1.printed_at IS NOT NULL AND tck1.printed_at >= d.date::date AND tck1.printed_at < d.date + '$interval days'::interval OR tck1.integrated_at IS NOT NULL AND tck1.integrated_at >= d.date::date AND tck1.integrated_at < d.date + '$interval days'::interval) AND tck1.duplicating IS NULL AND tck1.cancelling IS NULL AND tck1.id NOT IN (SELECT cancelling FROM ticket WHERE cancelling IS NOT NULL)) AS printed,
+            (
+              SELECT count(DISTINCT id) 
+              FROM (
+                SELECT tck1.id
+                FROM ticket_version tck1 $sFrom1 
+                WHERE $sWhere1 
+                (tck1.printed_at IS NOT NULL OR tck1.integrated_at IS NOT NULL)
+                AND tck1.duplicating IS NULL AND tck1.cancelling IS NULL 
+                AND tck1.id NOT IN (SELECT cancelling FROM ticket WHERE cancelling IS NOT NULL)
+                GROUP BY tck1.id
+                HAVING (
+                   min(tck1.printed_at) >= d.date::date AND min(tck1.printed_at) < d.date + '1 days'::interval 
+                OR min(tck1.integrated_at) >= d.date::date AND min(tck1.integrated_at) < d.date + '1 days'::interval
+                )
+              ) AS printed
+            ) AS printed,
             (SELECT count(DISTINCT tck2.id) FROM ticket tck2 ".str_replace('%%cpt%%', '2', $subfrom)." WHERE ".str_replace('%%cpt%%', '2', $subwhere)." tck2.printed_at IS NULL AND tck2.integrated_at IS NULL AND tck2.transaction_id IN (SELECT transaction_id FROM order_table WHERE updated_at >= d.date::date AND updated_at < d.date + '$interval days'::interval) AND tck2.duplicating IS NULL AND tck2.cancelling IS NULL AND tck2.id NOT IN (SELECT cancelling FROM ticket WHERE cancelling IS NOT NULL)) AS ordered,
             (SELECT count(DISTINCT tck3.id) FROM ticket tck3 ".str_replace('%%cpt%%', '3', $subfrom)." WHERE ".str_replace('%%cpt%%', '3', $subwhere)." tck3.created_at >= d.date::date AND tck3.created_at < d.date + '$interval days'::interval AND tck3.printed_at IS NULL AND tck3.integrated_at IS NULL AND tck3.transaction_id NOT IN (SELECT transaction_id FROM order_table) AND tck3.duplicating IS NULL AND tck3.cancelling IS NULL AND tck3.id NOT IN (SELECT cancelling FROM ticket WHERE cancelling IS NOT NULL)) AS asked,
             (SELECT count(DISTINCT tck4.id) FROM ticket tck4 ".str_replace('%%cpt%%', '4', $subfrom)." LEFT JOIN manifestation m ON m.id = tck4.manifestation_id WHERE ".str_replace('%%cpt%%', '4', $subwhere)." m.happens_at >= d.date::date AND m.happens_at < d.date + '$interval days'::interval AND (printed_at IS NOT NULL OR integrated_at IS NOT NULL) AND tck4.duplicating IS NULL AND cancelling IS NULL AND tck4.id NOT IN (SELECT cancelling FROM ticket WHERE cancelling IS NOT NULL)) AS passing
@@ -106,6 +134,8 @@ class activityActions extends sfActions
       break;
     }
     
+    sfContext::getInstance()->getLogger()->warning($q);
+        
     $pdo = Doctrine_Manager::getInstance()->getCurrentConnection()->getDbh();
     $stmt = $pdo->prepare($q);
     $stmt->execute();
