7dcdf40691e74b90b312957549dd42debb116f59
Fabrice-li <fabrice.petrequin@libre-informatique.fr>
2018-04-27 12:19:53 +0200

    (RP) - Add the creator name in the organism page - refs #6558 (#600)

--- apps/rp/modules/organism/templates/_show_dates.php
+++ apps/rp/modules/organism/templates/_show_dates.php
@@ -1,6 +1,6 @@
 <?php use_helper('Date') ?>
 <span class="tdp-dates sf_admin_form_field_dates">
-<span title="<?php echo __('Created at') ?>"><?php echo format_datetime($organism->created_at) ?></span>
+<span title="<?php echo __('Created at') ?>"><?php echo format_datetime($organism->created_at, 'f') ?> <?php echo __('by') ?> <strong><?php echo $organism->creator ?></strong></span>
 <br/>
-<span title="<?php echo __('Updated at') ?>"><?php echo format_datetime($organism->updated_at) ?></span>
+<span title="<?php echo __('Updated at') ?>"><?php echo format_datetime($organism->updated_at, 'f') ?> <?php echo __('by') ?> <strong><?php echo $organism->last_accessor ?></strong></span>
 </span>
