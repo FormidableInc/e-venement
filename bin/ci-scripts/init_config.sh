3f0e937a4b4551ec492953af7fc4f97a636854fd
Fanch <fanch.gerbes@gmail.com>
2018-05-03 11:31:24 +0200

    [CI] Add Deploy and Test to Pipeline (#606)
    * [CI] Rename jenkins pipeline file
    
    * [CI] Remove useless .env load
    
    * [CI] Add light version of jenkins pipeline
    
    * [CI] Add copy and Deploy Step
    
    * [CI] Try to run test
    
    * [CI] Syntax Fix
    
    * [CI] Fix Syntax

--- bin/ci-scripts/init_config.sh
+++ bin/ci-scripts/init_config.sh
@@ -1,18 +1,6 @@
 #!/usr/bin/env bash
 set -x
 
-# TODO share this between script (in an include)
-if [ -f .env ]
-then
-    source .env
-else
-    echo "Please run this script from project root, and check .env file as it is mandatory"
-    echo "If it is missing a quick solution is :"
-    echo "ln -s .env.travis .env"
-    exit 42
-fi
-
-# todo: get config from confd and/or private repo ?
 
 for i in config/autoload.inc.php config/project.yml config/extra-plugins.php
 do
--- bin/ci-scripts/submodule_init.sh
+++ bin/ci-scripts/submodule_init.sh
@@ -1,17 +1,6 @@
 #!/usr/bin/env bash
 set -x
 
-# TODO share this between script (in an include)
-if [ -f .env ]
-then
-    source .env
-else
-    echo "Please run this script from project root, and check .env file as it is mandatory"
-    echo "If it is missing a quick solution is :"
-    echo "ln -s .env.travis .env"
-    exit 42
-fi
-
 git submodule init
 git submodule update
 
