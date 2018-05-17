904b221f1ea761099f14ba8c0c2ec2fb95d397d7
Fabrice-li <fabrice.petrequin@libre-informatique.fr>
2018-05-17 15:59:43 +0200

    (pub) - Fix a bug when displaying remaining seats - refs #6669 (#612)

--- apps/pub/modules/manifestation/templates/_list_gauge.php
+++ apps/pub/modules/manifestation/templates/_list_gauge.php
@@ -10,7 +10,7 @@
   }
 
   if ( $manifestation->sold_tickets + $manifestation->online_limit >= $gauges ) {
-    $soldout = __('Sold Out');
+    $soldout = '<span class="closed">' . __('Sold Out') . '</span>';
   } else {
     $seats = $gauges - $manifestation->online_limit - $manifestation->sold_tickets;
     $soldout = format_number_choice('[1]%%SEATS%% available seat|(1,+Inf]%%SEATS%% available seats', array('%%SEATS%%' => $seats), $seats);
