9713b2a018d07dba16da9697a4aaa01cfb8ab305
Emmanuel Jacquet <emmanuel.jacquet@libre-informatique.fr>
2018-05-21 17:58:29 +0200

    [CI] Minor modification

---
+++ bin/ci-scripts/create_database.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+set -x
+
+# TODO share this between script (in an include)
+if [ -f .env ]
+then
+    source .env
+else
+    echo "Please run this script from project root, and check .env file as it is mandatory"
+    echo "If it is missing a quick solution is :"
+    echo "ln -s .env.travis .env"
+    exit 42
+fi
+
+if [ -z "${DBHOST}" ]
+then
+    echo "Please add DBHOST in .env file as it is mandatory"
+    exit 42
+fi
+
+# Check if database exist before drop and re-create
+#psql -w -h ${DBHOST} -U ${DBROOTUSER} -lqt | grep -w ${DBAPPUSER} | grep -w ${DBAPPNAME}
+#if [ $? -ne 0 ]
+#  then
+#psql -w -h ${DBHOST} -c "DROP DATABASE IF EXISTS ${DBAPPNAME};" -U ${DBROOTUSER}
+#psql -w -h ${DBHOST} -c "DROP ROLE IF EXISTS ${DBAPPUSER};" -U ${DBROOTUSER}
+#fi
+
+userexist=$(psql -qt -w -h ${DBHOST} -U ${DBROOTUSER} -c "SELECT rolname FROM pg_catalog.pg_roles WHERE rolname = '${DBAPPUSER}';"|sed -e s/' '//g)
+if [ -z ${userexist} ]
+then
+    psql -w -h ${DBHOST} -c "CREATE USER ${DBAPPUSER} WITH LOGIN PASSWORD '${DBAPPPASSWORD}';" -U ${DBROOTUSER}
+fi
+
+dbexist=$(psql -qt -w -h ${DBHOST} -U ${DBROOTUSER} -c "SELECT datname FROM pg_catalog.pg_database WHERE datname = '${DBAPPNAME}';"|sed -e s/' '//g)
+if [ -z ${dbexist} ]
+then
+    psql -w -h ${DBHOST} -c "CREATE DATABASE ${DBAPPNAME} OWNER ${DBAPPUSER};" -U ${DBROOTUSER}
+fi
+
+# NOT USED : psql -w -h ${DBHOST} -c 'CREATE EXTENSION IF NOT EXISTS "uuid-ossp";' -U ${DBROOTUSER} -d ${DBAPPNAME}
